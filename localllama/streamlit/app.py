86ba2dae5068d90aef97949bb91fb231a49c6abb
Mukesh Pilane <mukeshpilane@gmail.com>
2024-09-02 01:19:52 +0530

    streamlit deployment

---
+++ localllama/streamlit/app.py
@@ -0,0 +1,106 @@
+import streamlit as st
+from dotenv import load_dotenv
+import re
+import fitz
+import time
+from langchain_core.documents import Document
+from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from langchain.retrievers.document_compressors import FlashrankRerank
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+# Define the prompt template
+template = """You are a financial expert with access to the annual report of the company.
+When answering questions about the company's financial performance, prioritize information from the Financial Statements section.Considering the user's question, provide clear and concise answers from given context.
+{context}
+
+Question: {question}
+Answer:
+"""
+prompt = ChatPromptTemplate.from_template(template)
+
+load_dotenv()
+
+def main():
+    # st.header("Chat with PDF")
+    # embeddings = HuggingFaceEmbeddings(model_name="../all-MiniLM-L6-v2")
+    embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    chat_history = []
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3,stream=True,callbacks=[StreamingStdOutCallbackHandler()])
+    # Upload a PDF file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    if pdf is not None:
+        # Use Streamlit's session state to store the vector store and chat history
+        if "vector_store" not in st.session_state:
+            with st.spinner("Processing your PDF and creating the vector store..."):
+                start_time = time.time()
+                # Read and process the PDF
+                doc = fitz.open(stream=pdf.read(), filetype="pdf")
+                text_splitter = RecursiveCharacterTextSplitter(
+                    chunk_size=2000,
+                    chunk_overlap=200,
+                    length_function=len
+                )
+                pages = []
+                for page_no in range(doc.page_count):
+                    text = doc[page_no].get_text()
+                    text = re.sub(r"\n", " ", text)
+                    text = text_splitter.split_text(text=text)
+                    for chunk in text:
+                        page = Document(page_content=chunk, metadata={"page": page_no + 1})
+                        pages.append(page)
+
+                # Create the FAISS vector store and store it in session state
+                VectorStore = FAISS.from_documents(pages, embedding=embeddings)
+                st.session_state.vector_store = VectorStore
+                end_time = time.time()
+                processing_time = end_time - start_time
+                st.success(f"Vector store created successfully! {processing_time}")
+                
+            # Initialize chat history
+            if "chat_history" not in st.session_state:
+                st.session_state.chat_history = []
+
+        retriever = st.session_state.vector_store.as_retriever(search_kwargs={"k": 10})
+
+        Fcompressor = FlashrankRerank(top_n=4)
+        Flash_compression_retriever = ContextualCompressionRetriever(
+            base_compressor=Fcompressor, base_retriever=retriever
+        )
+
+        chain = ConversationalRetrievalChain.from_llm(
+            llm, Flash_compression_retriever, return_source_documents=True,
+            combine_docs_chain_kwargs={"prompt": prompt}
+        )
+        
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
+        if query:=st.chat_input("Ask questions about your PDF file:"):
+            st.session_state.messages.append({"role": "user", "content": query})
+            with st.chat_message("user"):
+                st.markdown(query)
+            with st.chat_message("assistant"):
+                stream = chain.invoke({"question": query, "chat_history": st.session_state.chat_history})
+                response = st.write(stream["answer"])
+                st.session_state.messages.append({"role": "assistant", "content": stream["answer"]})    
+if __name__ == '__main__':
+    main()
