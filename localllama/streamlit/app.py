add696c5f697b130a72375ef2664826dde01a458
Mukesh Pilane <mukeshpilane@gmail.com>
2024-09-04 15:24:14 +0530

    intro for streamlit app

--- localllama/streamlit/app.py
+++ localllama/streamlit/app.py
@@ -15,8 +15,8 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.retrievers.document_compressors import FlashrankRerank
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.callbacks.manager import CallbackManager
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 # Define the prompt template
 template = """You are a financial expert with access to the annual report of the company.
@@ -28,18 +28,45 @@ Answer:
 """
 prompt = ChatPromptTemplate.from_template(template)
 
+
+IntroTemplate = """Based on this data:
+{info}
+on the basis of the following report give Overview of the annual report in short
+overview:
+"""
+
+IntroPrompt = PromptTemplate.from_template(IntroTemplate)
 load_dotenv()
+
+
+def intro(VectorStore):
+    keyword = ["About","Financial Performance", "Letter of Ceo","Management Discussion"]
+    output=[]
+    for keyword in keyword:
+        doc = VectorStore.similarity_search(keyword, k=1)
+        output.append(doc[0].page_content)
+    return f"""{output[0]}\n
+{output[1]}\n
+{output[2]}\n
+{output[3]}\n
+"""
+
 
 def main():
     # st.header("Chat with PDF")
     # embeddings = HuggingFaceEmbeddings(model_name="../all-MiniLM-L6-v2")
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
-    chat_history = []
-    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3,stream=True,callbacks=[StreamingStdOutCallbackHandler()])
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3,stream=True)
     # Upload a PDF file
     pdf = st.file_uploader("Upload your PDF", type='pdf')
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
     if pdf is not None:
         # Use Streamlit's session state to store the vector store and chat history
@@ -68,10 +95,20 @@ def main():
                 end_time = time.time()
                 processing_time = end_time - start_time
                 st.success(f"Vector store created successfully! {processing_time}")
-                
+            
+                    # Initialize chat history
+            if "messages" not in st.session_state:
+                st.session_state.messages = []
+
             # Initialize chat history
             if "chat_history" not in st.session_state:
                 st.session_state.chat_history = []
+                
+            info = intro(VectorStore)
+            llmforIntro = LLMChain(prompt=IntroPrompt, llm=llm)
+            introduction = llmforIntro.run(info=info)
+            st.session_state.messages.append({"role": "assistant", "content":introduction})    
+
 
         retriever = st.session_state.vector_store.as_retriever(search_kwargs={"k": 10})
 
@@ -85,9 +122,6 @@ def main():
             combine_docs_chain_kwargs={"prompt": prompt}
         )
         
-        # Initialize chat history
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
 
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
